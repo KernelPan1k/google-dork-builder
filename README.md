4740a4379e03ac5c2ed9becd88079d130401af5e
micka <mickael.mathieu@kinitic.ch>
2019-06-18 08:55:08 +0200

    sync dorks with mozilla account + style

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 - Build your searches from the sidebar
 - Add search criteria from the context menu
+- Save your favorite dorks and synchronize them with your other devices
 - Download: https://addons.mozilla.org/fr/firefox/addon/google-dork-builder/
 
 
--- src/sidebar.js
+++ src/sidebar.js
@@ -3,33 +3,9 @@ const requestField = document.querySelector("#result-request");
 const dorksTable = document.querySelector("#dorks-storage");
 const dorksTableBody = dorksTable.querySelector("tbody");
 const editRow = document.querySelector("#edit");
+const storage = browser.storage.sync;
+
 let rowIdEdit = null;
-
-const operators = [
-  "filetype",
-  "site",
-  "intitle",
-  "allintitle",
-  "inurl",
-  "allinurl",
-  "intext",
-  "allintext",
-  "inanchor",
-  "allinanchor",
-  "cache",
-  "source",
-  "define",
-  "related",
-  "blogurl"
-];
-
-let options = "";
-
-for (let i = 0, l = operators.length; i < l; i++) {
-  options += `<option value="${ operators[i] }:">${ operators[i] }</option>`;
-}
-
-dorkList.innerHTML = options;
 
 const insertInField = (text) => {
   // https://stackoverflow.com/questions/1064089/inserting-a-text-where-cursor-is-using-javascript-jquery/1064139
@@ -52,69 +28,85 @@ const onError = (error) => {
   console.error(`Error: ${ error }`);
 };
 
-const getDorksSaved = () => browser.storage.sync.get();
+const getDorksInStorage = () => storage.get();
+
+const editDorkInStorage = (id, request) => getDorksInStorage().then(dorks => {
+  const list = dorks.list || [];
+
+  for (let i = 0, l = list.length; i < l; i++) {
+    if (list[i].id === id) {
+      list[i].request = request;
+    }
+  }
+
+  dorks.list = list;
+  storage.set(dorks);
+
+  populateTable();
+});
+
+const removeDorkInStorage = id => getDorksInStorage().then(dorks => {
+  let list = dorks.list || [];
+  list = list.filter(el => el.id !== id);
+  dorks.list = list;
+  storage.set(dorks);
+  populateTable();
+});
+
+const saveDorkInStorage = data => getDorksInStorage().then(dorks => {
+  const list = dorks.list || [];
+  list.push(data);
+  dorks.list = list;
+  storage.set(dorks);
+  populateTable();
+});
 
 const resetEdit = () => {
   rowIdEdit = null;
   editRow.setAttribute("disabled", "disabled");
 };
 
-const bindRun = () => {
-  Array.prototype.slice.call(dorksTable.querySelectorAll(".run-row"))
+const getRows = (selector, cb) => {
+  Array.prototype.slice.call(dorksTable.querySelectorAll(selector))
   .forEach(row => {
-    row.addEventListener('click', event => {
-      const parent = event.currentTarget.closest("tr");
-      const request = parent.querySelector("td").innerHTML.trim();
-
-      if (!request) {
-        return;
-      }
-
-      const url = "https://www.google.com/search?q=" + request;
-      browser.tabs.create({ url, active: true });
-    }, false);
+    row.addEventListener('click', cb, false);
   });
 };
 
-const bindRemove = () => {
-  Array.prototype.slice.call(dorksTable.querySelectorAll(".remove-row"))
-  .forEach(row => {
-    row.addEventListener('click', event => {
-      const parent = event.currentTarget.closest("tr");
-      const id = parent.getAttribute("data-row-id");
+const bindRun = event => {
+  const parent = event.currentTarget.closest("tr");
+  const request = parent.querySelector("td").innerHTML.trim();
 
-      getDorksSaved().then(dorks => {
-        let list = dorks.list || [];
-        list = list.filter(el => el.id !== id);
-        dorks.list = list;
-        browser.storage.sync.set(dorks);
-        populateTable();
-      });
-    }, false);
-  });
+  if (!request) {
+    return;
+  }
+
+  const url = "https://www.google.com/search?q=" + request;
+  browser.tabs.create({ url, active: true });
 };
 
-const bindEdit = () => {
-  Array.prototype.slice.call(dorksTable.querySelectorAll(".edit-row"))
-  .forEach(row => {
-    row.addEventListener('click', event => {
-      const parent = event.currentTarget.closest("tr");
-      rowIdEdit = parent.getAttribute("data-row-id");
-      requestField.value = parent.querySelector("td").innerHTML.trim();
-      editRow.removeAttribute("disabled");
-    }, false);
-  });
+const bindRemove = event => {
+  const parent = event.currentTarget.closest("tr");
+  const id = parent.getAttribute("data-row-id");
+  removeDorkInStorage(id);
+};
+
+const bindEdit = event => {
+  const parent = event.currentTarget.closest("tr");
+  rowIdEdit = parent.getAttribute("data-row-id");
+  requestField.value = parent.querySelector("td").innerHTML.trim();
+  editRow.removeAttribute("disabled");
 };
 
 const bindEvent = () => {
   resetEdit();
-  bindRun();
-  bindRemove();
-  bindEdit()
+  getRows(".edit-row", bindEdit);
+  getRows(".remove-row", bindRemove);
+  getRows(".run-row", bindRun);
 };
 
 const populateTable = () => {
-  getDorksSaved().then(dorks => {
+  getDorksInStorage().then(dorks => {
     const list = dorks.list || [];
     let rows = "";
 
@@ -122,9 +114,9 @@ const populateTable = () => {
       rows += `<tr data-row-id="${ list[i].id }">
                 <td>${ list[i].request }</td>
                 <td>
-                    <button class="edit-row">Edit</button>
-                    <button class="remove-row">Remove</button>
-                    <button class="run-row">Search</button>
+                    <a class="remove-row button"><img src="icons/delete.png" alt="delete" /></a>
+                    <a class="edit-row button"><img src="icons/edit.png" alt="edit" /></a>
+                    <a class="run-row button"><img src="icons/search.png" alt="search" /></a>
                 </td>
                </tr>`;
     }
@@ -135,6 +127,58 @@ const populateTable = () => {
   }).catch(onError);
 };
 
+const init = () => {
+  const operators = [
+    "filetype",
+    "site",
+    "intitle",
+    "allintitle",
+    "inurl",
+    "allinurl",
+    "intext",
+    "allintext",
+    "inanchor",
+    "allinanchor",
+    "cache",
+    "source",
+    "define",
+    "related",
+    "blogurl"
+  ];
+
+  let options = "";
+
+  for (let i = 0, l = operators.length; i < l; i++) {
+    options += `<option value="${ operators[i] }:">${ operators[i] }</option>`;
+  }
+
+  dorkList.innerHTML = options;
+
+  populateTable();
+};
+
+// browser.storage.sync.clear();
+
+document.querySelector("#save").addEventListener('click', () => {
+  const request = requestField.value.trim();
+  const id = generateId();
+
+  if (!request) {
+    return;
+  }
+
+  saveDorkInStorage({ id, request });
+}, false);
+
+editRow.addEventListener('click', () => {
+  const request = requestField.value.trim();
+
+  if (!request || !rowIdEdit) {
+    return;
+  }
+
+  editDorkInStorage(rowIdEdit, request);
+}, false);
 
 document.querySelector("#add-dork").addEventListener('click', () => {
   insertInField(dorkList.value)
@@ -157,45 +201,4 @@ browser.runtime.onMessage.addListener(request => {
   insertInField(operator + selectedText);
 });
 
-// browser.storage.sync.clear();
-
-document.querySelector("#save").addEventListener('click', () => {
-  const request = requestField.value.trim();
-
-  if (!request) {
-    return;
-  }
-
-  const id = generateId();
-
-  getDorksSaved().then(dorks => {
-    const list = dorks.list || [];
-    list.push({ id, request });
-    dorks.list = list;
-    browser.storage.sync.set(dorks);
-    populateTable();
-  }).catch(onError);
-}, false);
-
-editRow.addEventListener('click', () => {
-  const request = requestField.value.trim();
-
-  if (!request) {
-    return;
-  }
-
-  getDorksSaved().then(dorks => {
-    const list = dorks.list || [];
-    for (let i = 0, l = list.length; i < l; i++) {
-      if (list[i].id === rowIdEdit) {
-        list[i].request = request;
-      }
-    }
-    dorks.list = list;
-    browser.storage.sync.set(dorks);
-    populateTable();
-  }).catch(onError);
-}, false);
-
-
-populateTable();
+init();
